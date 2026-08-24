badafacf0a45b7d88d1d7867796b91ee91c7384b
Ratnesh919 <kumarsinghratnesh3@gmail.com>
2026-08-24 09:13:31 +0530

    chore: clean up obsolete test and scratch files and organize database migrations

--- test2.js
+++
@@ -1,9 +0,0 @@
-const str = `
-Here is your request:
-\`\`\`json
-{"action":"navigate","target":"urban"}
-{"action":"play_song","query":"Shape of You Ed Sheeran"}
-\`\`\`
-`;
-const re = /\{[^{}]*"action"\s*:\s*"(?:play_song|navigate|scroll|change_avatar|open_link)"[^{}]*\}/gi;
-console.log([...str.matchAll(re)].map(m => m[0]));
